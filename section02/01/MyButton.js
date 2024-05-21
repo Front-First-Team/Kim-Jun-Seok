6e65dd4207c49af9ef74d3266f2030b6d460ffb3
kimjunseok777 <wnszn5183@naver.com>
2024-05-21 20:51:33 +0900

    이프런 리액트

---
+++ section02/01/MyButton.js
@@ -0,0 +1,22 @@
+function MyButton(props) {
+    const [isClicked, setIsClicked] = React.useState(false)
+
+    return React.createElement(
+        'button',
+        {onClick: () => setIsClicked(true)},
+        isClicked ? 'Clicked!' : 'Click here!'
+    )
+}
+// 이 코드는 간단한 React의 함수 컴포넌트이다
+//--> React 컴포넌트가 이렇게 생겼구나 하고 넘어가자
+
+const domContainer = document.querySelector('#root')
+ReactDOM.render(React.createElement(MyButton), domContainer)
+// React DOM 에 렌더 함수를 사용해서 React 컴포넌트를 DOM container에 렌더링 하는 코드
+//--> 이 코드가 필요한 이유는 script를 사용해서 컴포넌트를 가져왔다고 해도
+//--> 컴포넌트가 화면에 보이는 것이 아니기 때문이다
+
+/*
+    이제 다시 브라우저를 열어서 click 버튼을 눌러보면 Clicked 로 바뀌는 것을 볼 수 있다
+    이것은 React Component에서 state가 바뀐 것이다
+*/
