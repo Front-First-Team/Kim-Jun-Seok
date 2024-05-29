83b9a8442662a64c5f8be162f53b2ea07645999b
kimjunseok777 <wnszn5183@naver.com>
2024-05-29 21:11:35 +0900

    react

--- free-practice-kjs/react-folder/src/InputSample.jsx
+++ free-practice-kjs/react-folder/src/InputSample.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
 function InputSample() {
-    const [text, setText] = useState('')
 
     function onChange(e) {
-        setText(e.target.value)
     }
 
     function onReset() {
-        setText('')
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input placeholder="이름" />
+            <input placeholder="닉네임" />
+
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                이름 (닉네임)
             </div>
         </div>
     )
