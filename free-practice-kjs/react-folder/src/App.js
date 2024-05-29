90c2389608ca5cdc805dd77c903ccd9c257d2d15
kimjunseok777 <wnszn5183@naver.com>
2024-05-29 16:12:05 +0900

    plus5

--- free-practice-kjs/react-folder/src/App.js
+++ free-practice-kjs/react-folder/src/App.js
@@ -2,15 +2,23 @@
 // import './App.css';
 import React from "react";
 import Hello from "./Hello";
+import './App.css'
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
+
   return (
     <div>
       <Hello />
-      <Hello />
-      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
     </div>
-    //--> 컴포넌트는 일종의 UI 조각이다. 이렇게 쉽게 재사용이 가능한 것도 확인 가능하다.
   );
 }
 
