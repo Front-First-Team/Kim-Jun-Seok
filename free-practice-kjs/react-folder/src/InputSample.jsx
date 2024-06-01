2a5f13876d69a1909ae0e32a0d1d335aa2372c67
kimjunseok777 <wnszn5183@naver.com>
2024-06-01 20:43:39 +0900

    react

--- free-practice-kjs/react-folder/src/InputSample.jsx
+++ free-practice-kjs/react-folder/src/InputSample.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function InputSample() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    })
+    const nameInput = useRef()
 
-    function onChange(e) {
-    }
+    const {name, nickname} = inputs
 
-    function onReset() {
-    }
+    const onChange = (e) => {
+        const{value, name} = e.target
+        setInputs({
+            ...inputs,
+            [name]: value
+        })
+    };
+  
+    const onReset = () => {
+        setInputs({
+            name: '',
+            nickname: ''
+        })
+        nameInput.current.focus()
+    };
 
     return (
         <div>
-            <input placeholder="이름" />
-            <input placeholder="닉네임" />
-
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                이름 (닉네임)
+                {name} ({nickname})
             </div>
         </div>
-    )
+    );
 }
 
 export default InputSample
